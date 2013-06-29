5bd42c50d27b1f48926cf6c5c0e38fd28a7b990c
Harley Hauer <harley.hauer@gmail.com>
2013-06-28 19:07:15 -0700

    Added custom manager for ProductionEvent to make instantiating them reliably easier. Laboratory now shows production in queue and completes appropriate ProductionEvents.

--- Laboratory/admin.py
+++ Laboratory/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from Laboratory.models import Element, Resource
+from Laboratory.models import Element, Resource, ProductionEvent
 
 admin.site.register(Element)
 admin.site.register(Resource)
+admin.site.register(ProductionEvent)
--- Laboratory/urls.py
+++ Laboratory/urls.py
@@ -6,4 +6,6 @@ urlpatterns = patterns('',
     url(r'^periodic_table/$', views.periodic_table),
     url(r'^periodic_table/(?P<element>\w+)/$', views.element_detail),
     url(r'^inventory/$', views.inventory),
+    
+    url(r'^test/$', views.debug_test_production),
 )
